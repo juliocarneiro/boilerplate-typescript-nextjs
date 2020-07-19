1ba01fe956a7915c602f65f04d7e7fb1216caecf
juliocarneiro <juliocarneiro@gmail.com>
2020-07-19 16:34:31 -0300

    update snapshots and jest-styled-components plugin

--- src/components/Main/index.tsx
+++ src/components/Main/index.tsx
@@ -1,7 +1,11 @@
+import * as S from './styles'
+
 const Main = () => (
-  <main>
-    <h2>teste</h2>
-  </main>
+  <S.Wrapper>
+    <S.Title>Boilerplate</S.Title>
+    <S.Description>TypeScript, ReactJS, NextJS e Styled-Components</S.Description>
+    <S.Illustration alt="Logo" src="/img/logo.svg" />
+  </S.Wrapper>
 )
 
 export default Main
